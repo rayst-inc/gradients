503e36eb652daf3d599504299e7950002b6bfe70
rayst <a@ray.st>
2022-11-02 16:16:03 +0700

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { NextSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 import { SEO } from 'src/lib/next-seo'
 import 'styles/globals.css'
+
+const isProd = () => process.env.NODE_ENV === 'production'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,6 +19,14 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <NextSeo {...SEO} />
       <Component {...pageProps} />
+      {isProd() && (
+        <Script
+          async
+          defer
+          data-website-id="1399c1da-1ee7-4c74-a87b-747bacbe1785"
+          src="https://u-ma.vercel.app/u.js"
+        ></Script>
+      )}
     </>
   )
 }
