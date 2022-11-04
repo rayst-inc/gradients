b77245d3ceb32e86b97cb54ccfd5c50dbb4abe8c
Daun <iam@daun.io>
2022-11-04 20:44:05 +0900

    feat: add emojis footer navigation (#6)
    Co-authored-by: Daun <iam@nyanye.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,24 @@
 export function Footer() {
   return (
-    <div className={`grid h-16 w-full place-content-center`}>
+    <div className={`mb-16 flex w-full flex-row place-content-center`}>
       <a
         href="https://ray.st"
         target={'_blank'}
         rel="noreferrer"
-        className="umami--click--footer-rayst opacity-50 hover:underline hover:opacity-80"
+        className="umami--click--footer-rayst mr-8 opacity-50 hover:underline hover:opacity-80"
       >
         ray.st
+      </a>
+      <span className="text-bold mr-2 bg-gradient-to-r from-amber-100 to-amber-400 bg-clip-text text-xs text-transparent">
+        {'new '}
+      </span>
+      <a
+        href="https://emojis.ray.st"
+        target={'_blank'}
+        rel="noreferrer"
+        className="umami--click--footer-emoji-rayst opacity-50 hover:underline hover:opacity-80"
+      >
+        emojis
       </a>
     </div>
   )
