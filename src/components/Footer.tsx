123a71f579390a0c93be4a13c87b3bcee1204e9d
rayst <a@ray.st>
2022-11-05 09:24:20 +0700

    disable noreferrer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export function Footer() {
   return (
     <div className={`mb-16 flex w-full flex-row place-content-center`}>
+      {/* eslint-disable-next-line react/jsx-no-target-blank */}
       <a
         href="https://ray.st"
         target={'_blank'}
-        rel="noreferrer"
         className="umami--click--footer-rayst mr-8 opacity-50 hover:underline hover:opacity-80"
       >
         ray.st
@@ -12,11 +12,11 @@ export function Footer() {
       <span className="text-bold mr-2 bg-gradient-to-r from-amber-100 to-amber-400 bg-clip-text text-xs text-transparent">
         {'new '}
       </span>
+      {/* eslint-disable-next-line react/jsx-no-target-blank */}
       <a
         href="https://emojis.ray.st"
         target={'_blank'}
-        rel="noreferrer"
-        className="umami--click--footer-emoji-rayst opacity-50 hover:underline hover:opacity-80"
+        className="umami--click--footer-emojis opacity-50 hover:underline hover:opacity-80"
       >
         emojis
       </a>
